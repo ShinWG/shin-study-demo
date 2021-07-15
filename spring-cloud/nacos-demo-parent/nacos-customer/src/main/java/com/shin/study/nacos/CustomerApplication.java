fe911178880f75008e905f3d874b6cc85b0be1d8
yjwb_fanao <yjwb_fanao@yunjingtech.cn>
2021-07-15 17:56:31 +0800

    nacos 服务注册 服务消费实现

---
+++ spring-cloud/nacos-demo-parent/nacos-customer/src/main/java/com/shin/study/nacos/CustomerApplication.java
@@ -0,0 +1,47 @@
+package com.shin.study.nacos;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @description:
+ * @author: fanAo
+ * @date: 2021/7/15 17:21
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CustomerApplication {
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+    
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
+
+    @RestController
+    class CustomerController {
+
+        private final RestTemplate restTemplate;
+
+        @Autowired
+        public CustomerController(RestTemplate restTemplate) {this.restTemplate = restTemplate;}
+
+        @RequestMapping(value = "/echo/{str}", method = RequestMethod.GET)
+        public String echo(@PathVariable String str) {
+            return restTemplate.getForObject("http://nacosProvider/echo/" + str, String.class);
+        }
+    }
+}
--- spring-cloud/nacos-demo-parent/nacos-provider/src/main/java/com/shin/study/nacos/ProviderApplication.java
+++ spring-cloud/nacos-demo-parent/nacos-provider/src/main/java/com/shin/study/nacos/ProviderApplication.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,18 +23,28 @@ public class ProviderApplication {
         SpringApplication.run(ProviderApplication.class, args);
     }
 
+    // 动态配置 controller
     @RestController
     @RequestMapping("/config")
     // 实时刷新配置
     @RefreshScope
     class ConfigController {
-
         @Value("${useLocalCache:false}")
         private boolean useLocalCache;
 
         @RequestMapping("/get")
         public boolean get() {
                 return useLocalCache;
+        }
+    }
+
+    // 服务注册Demo
+    @RestController
+    class ProviderConfigController {
+
+        @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+        public String echo(@PathVariable String string) {
+            return "Hello Nacos Discovery " + string;
         }
     }
 }
